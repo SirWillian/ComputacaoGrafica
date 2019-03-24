862583f3fd1526e99a449748e572c1d98f6ab913
SirWillianH <wsimanbrazil@yahoo.com.br>
2019-03-24 15:25:35 -0300

    Added example library

---
+++ 2019cglib/include/cg.h
@@ -0,0 +1,16 @@
+/**
+ * @file cg.h
+ * @brief cglib includes.
+ * @author Ricardo Dutra da Silva
+ */
+
+
+#ifndef _CG_H_
+#define _CG_H_
+ 
+
+#include <cgTypes.h>
+#include <cgPrimitives.h>
+
+
+#endif /* _CG_H_ */
