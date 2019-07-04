5fb9a70751c14fc9a7eca6be0a0a938fa9f47a43
Willian Lima <wsimanbrazil@yahoo.com.br>
2019-07-04 18:20:54 -0300

    Fixed reverted normals

--- CGlib/applications/trabalho2/1717553/Triangle.py
+++ CGlib/applications/trabalho2/1717553/Triangle.py
@@ -4,7 +4,7 @@ class Triangle(object):
     def __init__(self,vertices):
         self.vertices=vertices
         # For P, Q, R, defined counter-clockwise, glm.cross(R-Q, P-Q)
-        RQ = glm.vec3(vertices[0].x-vertices[1].x,vertices[0].y-vertices[1].y,vertices[0].z-vertices[1].z)
-        PQ = glm.vec3(vertices[2].x-vertices[1].x,vertices[2].y-vertices[1].y,vertices[2].z-vertices[1].z)
-        normal = glm.normalize(glm.cross(RQ,PQ))*0.1
+        PQ = glm.vec3(vertices[0].x-vertices[1].x,vertices[0].y-vertices[1].y,vertices[0].z-vertices[1].z)
+        RQ = glm.vec3(vertices[2].x-vertices[1].x,vertices[2].y-vertices[1].y,vertices[2].z-vertices[1].z)
+        normal = glm.normalize(glm.cross(RQ,PQ))
         self.normal = [normal.x,normal.y,normal.z]
